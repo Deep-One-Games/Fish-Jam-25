cd5a6117c7e2e42d9a56ce75293947bc24da1e90
Elena Stroiu <80272300+EStroiu@users.noreply.github.com>
2025-08-06 11:36:00 +0200

    changed placeholder in the inventory

--- src/controllers/fpcontroller.gd
+++ src/controllers/fpcontroller.gd
@@ -12,7 +12,7 @@ var mouse_captured: bool = false
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var move_dir: Vector2 # Input direction for movement
-var look_dir: Vector2 # Input direction for look/aim
+var look_dir: Vector2 # 1put direction for look/aim
 
 var walk_vel: Vector3 # Walking velocity 
 var grav_vel: Vector3 # Gravity velocity 
