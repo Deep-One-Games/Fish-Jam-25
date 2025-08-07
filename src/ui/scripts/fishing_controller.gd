fed15ea5636ce3539de38a4615ffdb59e22f8362
Elena Stroiu <80272300+EStroiu@users.noreply.github.com>
2025-08-07 12:57:12 +0200

    fixed promts hide + unhide when in fishing mode

--- src/ui/scripts/fishing_controller.gd
+++ src/ui/scripts/fishing_controller.gd
@@ -1,7 +1,9 @@
+# FishingController.gd
 extends Node
 
-@export var player: NodePath 
-@export var ui_scene: PackedScene
+@export var player: NodePath            # your FPController
+@export var ui_scene: PackedScene       # fishing.tscn
+@export var player_ui_path: NodePath    # path to your PlayerUI node
 
 var fishing_ui: Control
 var fps_controller: FPController
@@ -15,6 +17,13 @@ func enter_fishing_mode() -> void:
 	fishing_ui.connect("exit_fishing", Callable(self, "_on_exit_fishing"))
 
 func _on_exit_fishing() -> void:
+	# remove the fishing UI
 	fishing_ui.queue_free()
 
+	# re-enable the normal FPS controller
 	fps_controller.disable = false
+
+	# tell PlayerUI to put the fish prompt back if needed
+	var ui = get_node(player_ui_path) as Node
+	if ui and ui.has_method("reset_fish_prompt"):
+		ui.call("reset_fish_prompt")
