98654a92615ed05430a49ea11a04eb9a6ab1b8c2
Elena Stroiu <80272300+EStroiu@users.noreply.github.com>
2025-08-07 11:45:10 +0200

    changed the exit key from fishing mode

--- src/scenes/fishing_mode.gd
+++ src/scenes/fishing_mode.gd
@@ -1,6 +1,14 @@
 extends Control
 signal exit_fishing
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+func _ready() -> void:
+	# Ensure this Control grabs input focus
+	focus_mode = FocusMode.FOCUS_ALL
+	grab_focus()
+	# Make sure unhandled input is processed
+	set_process_unhandled_input(true)
+
+func _input(event: InputEvent) -> void:
+	# Listen for your new action
+	if event.is_action_pressed("exit_fish"):
 		emit_signal("exit_fishing")
