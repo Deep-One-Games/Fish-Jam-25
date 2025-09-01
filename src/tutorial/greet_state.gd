9a028ec46e7c2be8e5d10c62d9c91797c1a353f8
Emil Choparinov <emilchoparinov@gmail.com>
2025-09-01 07:28:30 +0200

    adding whistling

--- src/tutorial/greet_state.gd
+++ src/tutorial/greet_state.gd
@@ -2,6 +2,8 @@ extends FSMState
 
 @export var jack: JackNPC
 
+@export var audio: AudioStreamPlayer3D
+
 func enter() -> void:
 	jack.path.curve = jack.c_home_to_lake
 	Storage.save_process()
@@ -13,3 +15,8 @@ func enter() -> void:
 	tfsm.player_sensor.dialogue = jack.profile.npc_dialogue
 	tfsm.player_sensor.title = "JACK_start"
 	tfsm.player_sensor.npc = jack.profile
+	audio.play()
+
+func exit() -> void:
+	audio.stop()
+
